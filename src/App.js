98dd055bf6f4f675eb673671c83ebb4c93c58b9b
Dextrodark <faizalmohamed5302002@gmail.com>
2023-08-30 12:21:48 +0530

    Added basic structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const App = () => {
+  return (
+    <div className=''>
+      <Fade top>
+        Its working
+      </Fade>
+    </div>
+  )
+}
+
+export default App
